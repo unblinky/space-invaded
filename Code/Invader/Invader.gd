28482480a89b0137a39de0f8c443861b296f89ee
unblinky <12799346-unblinky@users.noreply.gitlab.com>
2024-03-15 00:15:00 -0700

    Fleet interaction needs work.

--- Code/Invader/Invader.gd
+++ Code/Invader/Invader.gd
@@ -1,31 +1,26 @@
 extends Area3D
 class_name Invader
 
-var row_height = 1.0 # meters.
-var speed: float = 5.0 # m / sec.
-var bounds: float = 10.0
 var score_board: ScoreBoard
+var main: Main
+var speed: float = 5.0 # m / sec.
+var points: int = 13
 
 func _ready():
 	area_entered.connect(OnAreaEntered)
 
+
 func _process(delta):
 	position.x += speed * delta
-	
-	if position.x > bounds:
-		position.x = bounds
-		speed *= -1
-		position.y -= row_height
-	
-	if position.x < -bounds:
-		position.x = -bounds
-		speed *= -1
-		position.y -= row_height
+
 
 func OnAreaEntered(other_area: Area3D):
 	if other_area is Lazer:
-		score_board.UpdateScore(13)
+		score_board.UpdateScore(points)
+		main.invaders.erase(other_area) # TODO: Not erasing.
 		other_area.queue_free()
+		#call_deferred("queue_free")
 		queue_free()
 	if other_area is Ship:
 		print("GameOver / Ship life?")
+		main.game_over.show()
--- Code/Main/Main.gd
+++ Code/Main/Main.gd
@@ -2,16 +2,49 @@ extends Node
 class_name Main
 
 const INVADER = preload("res://Invader/Invader.tscn")
+
 @onready var score_board = $ScoreBoard
+@onready var play_button = $GameOver/VBox/PlayButton
+@onready var game_over = $GameOver
+
+var invaders: Array
+var bounds: float = 10.0
+var row_height = 1.5 # meters.
 
 func _ready():
+	get_tree().change_scene_to_file("res://ScoreBoard/ScoreBoard.tscn")
+	play_button.pressed.connect(OnPlayPressed)
+	game_over.hide()
 	SpawnInvader()
 
+func OnPlayPressed():
+	get_tree().reload_current_scene()
+
+
 func SpawnInvader():
-	# HACK: Do this better!
+	invaders.clear()
+	
 	for i in 5:
 		var invader = INVADER.instantiate()
 		invader.score_board = score_board
-		invader.position.x += i * 1.5
+		invader.main = self
+		invader.position.x += i * row_height
 		invader.position.y = 5
+		invaders.append(invader)
 		add_child(invader)
+
+func _process(delta):
+	for invader in invaders:
+		if invader != null:
+			if invader.position.x > bounds:
+				Drop()
+		
+			if invader.position.x < -bounds:
+				Drop()
+
+func Drop():
+	for invader in invaders:
+		if invader != null:
+			invader.speed *= -1
+			invader.position.y -= row_height
+
